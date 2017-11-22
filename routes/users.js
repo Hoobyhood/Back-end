d0f5cd2514d4c87e32640288202f5b99d5094d52
Beshr GH <beshr.g@gmail.com>
2017-11-22 21:07:43 +0200

    All Working

--- routes/users.js
+++ routes/users.js
@@ -21,44 +21,31 @@ userRouter.route('/')
 
 .get(function (req, res, next) { 
     console.log('get is getted');
-    res.json('Will send all the Profiles to you!');
-    db.collection('Users').find({},function(err,profiles){
+    //res.json('Will send all the Profiles to you!');
+    db.collection('Users').find({}).toArray(function(err,result){
         if (err) throw err;
-        console.log(profiles);
+        console.log(result);
+        res.json(result);
+        
     });
+    
     
     })
 
 .post(function (req, res, next) { 
     console.log('post is posted');
-    res.json('Will add the Profile: ' + req.body.username + ' with details: ' + req.body.Age);
+    //res.json('Will add the Profile: ' + req.body.username + ' with details: ' + req.body.Age);
     var USER = new Users ({username:req.body.username ,password:req.body.password , Age:req.body });
     console.log('Created USER');
     console.log(USER);
     db.collection('Users').insertOne({USER}, function(err, result) {
         assert.equal(err, null);
         console.log("Inserted a newUser into the Users collection.");
+        res.json('added the newUser \n' + USER);
+        
         
       });
-    /*USER.save((err ,newUSER)=>{
-        if (err) throw err;
 
-        res.status(201);
-        console.log("After Insert ");
-        res.json('After Insert');
-        res.json(newUSER);
-        console.log(newUSER); 
-       
-    });
-    */
-    //users.insertOne({username:req.body.username , passward: req.body.passward ,Age : req.body.Age}, function 
-    /*(err,result){ 
-        assert.equal(err,null); 
-        console.log("After Insert:");
-        res.json(result.ops);
-       
-    
-});*/
 }) ;
 /*db.dropCollection("users", function(err, result){ 
     assert.equal(err,null); 
